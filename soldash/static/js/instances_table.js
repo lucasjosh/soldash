7661e633ffb478e3849d316d1ccd34e5188bf756
Aengus Walton <ventolin@gmail.com>
2011-10-11 15:16:58 +0200

    Dashboard icons now working correctly

--- soldash/static/js/instances_table.js
+++ soldash/static/js/instances_table.js
@@ -1,12 +1,25 @@
-function command_click(command, solr_host, solr_port) {
+function command_click(command, solr_host, solr_port, element_id) {
+	changeIcon(element_id, "working");
 	$.ajax({
 	  url: "/execute/" + command,
 	  type: "GET",
 	  data: "host=" + solr_host + "&port=" + solr_port,
-	  success: handleResponse
+	  success: function(data, status, jqXHR){
+		handleResponse(command, data, status, solr_host, solr_port, element_id);
+		}
 	});
 }
 
-function handleResponse(data, status, jqXHR) {
+function handleResponse(command, data, status, solr_host, solr_port, element_id) {
 	console.log(data);
+	if(data['status'] == 'ERROR') {
+		changeIcon(element_id, "error");
+	} else if(data['status'] == 'OK') {
+		changeIcon(element_id, "success");
+	}
+}
+
+function changeIcon(element_id, new_icon) {
+	$(element_id).removeClass("ready success error working");
+	$(element_id).addClass(new_icon);
 }
